20b23880a51927d31a177d3af958182096d15cf4
TheBigEye <nahuelsenek@gmail.com>
2021-02-14 22:29:14 -0300

    Update Zombie.java

--- src/minicraft/entity/mob/Zombie.java
+++ src/minicraft/entity/mob/Zombie.java
@@ -9,7 +9,7 @@ public class Zombie extends EnemyMob {
 	static {
 		sprites = new MobSprite[4][4][2];
 		for (int i = 0; i < 4; i++) {
-			MobSprite[][] list  = MobSprite.compileMobSpriteAnimations(8, 0 + (i * 2));
+			MobSprite[][] list = MobSprite.compileMobSpriteAnimations(8, 0 + (i * 2));
 			sprites[i] = list;
 		}
 	}
@@ -29,7 +29,7 @@ public class Zombie extends EnemyMob {
 	
 	public void die() {
 		if (Settings.get("diff").equals("Easy")) dropItem(2, 4, Items.get("cloth"));
-		if (Settings.get("diff").equals("Normal")) dropItem(2, 3, Items.get("cloth"));
+		if (Settings.get("diff").equals("Normal")) dropItem(1, 3, Items.get("cloth"));
 		if (Settings.get("diff").equals("Hard")) dropItem(1, 2, Items.get("cloth"));
 		
 		if(random.nextInt(60) == 2) {
