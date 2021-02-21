ef4a6ecbd929d7a6637242bd01530825f0c9ed83
TheBigEye <nahuelsenek@gmail.com>
2021-02-21 09:33:28 -0300

    Lock the world editor when making a multiplayer world

--- src/minicraft/screen/PauseDisplay.java
+++ src/minicraft/screen/PauseDisplay.java
@@ -27,8 +27,8 @@ public class PauseDisplay extends Display {
 		entries.addAll(Arrays.asList(
 			new BlankEntry(),
 			new SelectEntry("Return to Game", () -> Game.setMenu(null)),
-			new SelectEntry("Options", () -> Game.setMenu(new OptionsDisplay())),
-			new SelectEntry("World", () -> Game.setMenu(new WorldInfoDisplay()))
+			new SelectEntry("Options", () -> Game.setMenu(new OptionsDisplay()))
+			//new SelectEntry("World", () -> Game.setMenu(new WorldInfoDisplay()))
 			));
 		
 		if(!Game.ISONLINE) {
@@ -39,6 +39,7 @@ public class PauseDisplay extends Display {
 		}
 		
 		if(!Game.isValidClient()) {
+			entries.add(new SelectEntry("World", () -> Game.setMenu(new WorldInfoDisplay())));
 			entries.add(new SelectEntry("Save Game", () -> {
 				Game.setMenu(null);
 				if(!Game.isValidServer())
