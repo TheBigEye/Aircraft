9e7d2b28a4af38004b496cb4c27239ed3f7b9983
TheBigEye <thebigeyedev@gmail.com>
2021-03-18 19:39:30 -0300

    Fix to a critical bug .. Villagers can now spawn

--- src/minicraft/entity/mob/villager/VillagerMob.java
+++ src/minicraft/entity/mob/villager/VillagerMob.java
@@ -12,71 +12,76 @@ import minicraft.level.tile.Tiles;
 
 public class VillagerMob extends MobAi {
 	protected int color;
-	
+
 	/**
-	 * Constructor for a non-hostile (Villager) mob.
-	 * healthFactor = 3.
+	 * Constructor for a non-hostile (Villager) mob. healthFactor = 3.
+	 * 
 	 * @param sprites The mob's sprites.
 	 */
 	public VillagerMob(MobSprite[][] sprites) {
 		this(sprites, 3);
 	}
-	
+
 	/**
 	 * Constructor for a non-hostile (Villager) mob.
-	 * @param sprites The mob's sprites.
-	 * @param healthFactor Determines the mobs health. Will be multiplied by the difficulty
-	 * and then added with 5.
+	 * 
+	 * @param sprites      The mob's sprites.
+	 * @param healthFactor Determines the mobs health. Will be multiplied by the
+	 * difficulty and then added with 5.
 	 */
 	public VillagerMob(MobSprite[][] sprites, int healthFactor) {
-		super(sprites, 5 + healthFactor * Settings.getIdx("diff"), 5*60*Updater.normSpeed, 45, 40);
+		super(sprites, 5 + healthFactor * Settings.getIdx("diff"), 5 * 60 * Updater.normSpeed, 45, 40);
 	}
-	
+
 	@Override
 	public void render(Screen screen) {
 		super.render(screen);
 	}
-	
+
 	@Override
 	public void randomizeWalkDir(boolean byChance) {
-		if(xa == 0 && ya == 0 && random.nextInt(5) == 0 || byChance || random.nextInt(randomWalkChance) == 0) {
+		if (xa == 0 && ya == 0 && random.nextInt(5) == 0 || byChance || random.nextInt(randomWalkChance) == 0) {
 			randomWalkTime = randomWalkDuration;
 			// multiple at end ups the chance of not moving by 50%.
 			xa = (random.nextInt(3) - 1) * random.nextInt(2);
 			ya = (random.nextInt(3) - 1) * random.nextInt(2);
 		}
 	}
-	
+
 	public void die() {
 		super.die(15);
 	}
-	
+
 	/**
 	 * Checks a given position in a given level to see if the mob can spawn there.
 	 * Passive mobs can only spawn on grass or flower tiles.
+	 * 
 	 * @param level The level which the mob wants to spawn in.
-	 * @param x X map spawn coordinate.
-	 * @param y Y map spawn coordinate.
+	 * @param x     X map spawn coordinate.
+	 * @param y     Y map spawn coordinate.
 	 * @return true if the mob can spawn here, false if not.
 	 */
 	public static boolean checkStartPos(Level level, int x, int y) {
-		
+
 		int r = (Game.isMode("score") ? 22 : 15) + (Updater.getTime() == Updater.Time.Night ? 0 : 10); // get no-mob radius by
 
-		if (!MobAi.checkStartPos(level, x, y, 128, r))
+		if (!MobAi.checkStartPos(level, x, y, 16, r))
 			return false;
-		
+
 		Tile tile = level.getTile(x >> 4, y >> 4);
-		if ( tile != Tiles.get("Wood Planks") && tile != Tiles.get("wood planks") && tile != Tiles.get("Path") && tile != Tiles.get("path")) {
+		if (tile != Tiles.get("Wood Planks") && tile != Tiles.get("wood planks") && tile != Tiles.get("Path")
+				&& tile != Tiles.get("path")) {
 			return false;
-		} else if (tile != Tiles.get("Grass") && tile != Tiles.get("Sand") && tile != Tiles.get("Snow") && tile != Tiles.get("birch tree") && tile != Tiles.get("tree") && tile != Tiles.get("flower") && tile != Tiles.get("water") && tile != Tiles.get("wheat") && tile != Tiles.get("farmland")) {
-		
-		} return true;
+		} else if (tile != Tiles.get("Grass") && tile != Tiles.get("Sand") && tile != Tiles.get("Snow")
+				&& tile != Tiles.get("birch tree") && tile != Tiles.get("tree") && tile != Tiles.get("flower")
+				&& tile != Tiles.get("water") && tile != Tiles.get("wheat") && tile != Tiles.get("farmland")) {
+			return true;
+		} else
+			return false;
 	}
-	
+
 	@Override
 	public int getMaxLevel() {
-		return 3;
+		return 6;
 	}
 }
-
