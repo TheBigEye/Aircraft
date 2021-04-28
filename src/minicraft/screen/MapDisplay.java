3964d5242023a46a2aa6b1ffe1200af91c8137b1
TheBigEye <thebigeyedev@gmail.com>
2021-04-28 11:26:33 -0300

    Resolve conflicts

--- src/minicraft/screen/MapDisplay.java
+++ src/minicraft/screen/MapDisplay.java
@@ -10,11 +10,14 @@ import minicraft.level.tile.Tile;
 
 public class MapDisplay extends Display {
 
+	private static final int PLAYER_MARKER_SPRITE = 2 + 12 * 32;
+	private static final int PLAYER_MARKER_COLOR = Color.get(-1, 255, 0, 0);
+	
 	public MapDisplay() {
 
 		Menu.Builder builder = new Menu.Builder(true, 0, RelPos.CENTER);
 		builder.setTitle("map");
-		builder.setSize(148, 148);
+		builder.setSize(140, 140);
 		builder.setFrame(443, 1, 443);
 
 		menus = new Menu[1];
@@ -25,78 +28,48 @@ public class MapDisplay extends Display {
 
 	@Override
 	public void tick(InputHandler input) {
-		if (input.getKey("menu").clicked || input.getKey("attack").clicked || input.getKey("exit").clicked)
+		if (input.getKey("menu").clicked || input.getKey("attack").clicked || input.getKey("exit").clicked) {
 			Game.exitMenu();
+		}
 	}
 
 	@Override
 	public void render(Screen screen) {
-		menus[0].render(screen);
+		Menu menu = menus[0];
+		menu.render(screen);
 
-		Level level = Game.levels[Game.currentLevel];
+		// Player Tile Coordinates
+		int ptx = Game.player.x >> 4;
+		int pty = Game.player.y >> 4;
 
-		Rectangle menuBounds = menus[0].getBounds();
+		/*
+		 * This is for worlds large than 128x128
+		 * due that map can just display 128x128 pixels
+		 * thus we can fix position to get tiles correctly
+		 *
+		 * To explain this, we divide by 128 to get an integer about how many tiles
+		 * we have skipped, once done that, we can multiply 128 again to shift those
+		 * coordinates, keeping in mind that, smx != ptx and smy != pty, due that those
+		 * operations didn't use decimal part
+		 *
+		 * smx and smy mean Shift Map X and Shift Map Y
+		 */
+		int smx = (ptx >> 7) << 7;
+		int smy = (pty >> 7) << 7;
 
-		// used for world sizes bigger than 128, since the map can only render 128x128
-		// tiles
-		int[] offset = new int[2];
-		offset[0] = 0;
-		offset[1] = 0;
+		Rectangle menuBounds = menu.getBounds();
 
-		// player tile coords
-		int ptx = (Game.player.x) / 16;
-		int pty = (Game.player.y) / 16;
+		for (int y = 0; y < 128; y++) {
+			for (int x = 0; x < 128; x++) {
+				MapData mapData = MapData.getById(Game.levels[Game.currentLevel].getTile(x + smx, y + smy).id);
+				int color = mapData != null ? mapData.color : 0;
 
-		// determines which 128x128 "chunk" the player is in
-		if (level.w == 256) {
-			if (ptx >= 128) {
-				offset[0] = 1;
-			}
-			if (pty >= 128) {
-				offset[1] = 1;
-			}
-		}	
-
-			if (level.w == 512) {
-				if (ptx >= 128 && ptx < 256) {
-					offset[0] = 1;
-				} else if (ptx >= 256 && ptx < 385) {
-					offset[0] = 2;
-				} else if (ptx >= 385 && ptx < 512) {
-					offset[0] = 3;
-				}
-			}	
-
-			if (pty >= 128 && pty < 256) {
-				offset[1] = 1;
-			} else if (pty >= 256 && pty < 385) {
-				offset[1] = 2;
-			} else if (pty >= 385 && pty < 512) {
-				offset[1] = 3;
-			}
-		
-
-		for (int i = 0; i < 136; i++) {
-			for (int c = 0; c < 136; c++) {
-				int color = 0;
-				Tile tile = level.getTile(i, c);
-
-				MapData mapData = MapData.getById(tile.id);
-				if (mapData != null) {
-					color = mapData.color;
-				}
-				// by drawing with only one pixel at a time we can draw with much more precision
-
-				if (level.w == 128) {
-					screen.setPixel(i + menuBounds.getLeft() + 10, c + menuBounds.getTop() + 10, color);
-				} else {
-					screen.setPixel(i + menuBounds.getLeft() + 6, c + menuBounds.getTop() + 6, color);
-				}
+				screen.setPixel(x + menuBounds.getLeft() + 6, y + menuBounds.getTop() + 6, color);
 			}
 		}
 
-		// render the marker for the player
-		screen.render((Game.player.x - 8) / 16 + menuBounds.getLeft() + 2 - (offset[0] * 128), (Game.player.y - 8) / 16 + menuBounds.getTop() + 2 - (offset[1] * 128), 2 + 12 * 32,
-				Color.get(-1, 255, 0, 0));
+		// Render the marker for the player
+		screen.render(ptx % 128 + menuBounds.getLeft() + 2, pty % 128 + menuBounds.getTop() + 2,
+				MapDisplay.PLAYER_MARKER_SPRITE, MapDisplay.PLAYER_MARKER_COLOR);
 	}
 }
