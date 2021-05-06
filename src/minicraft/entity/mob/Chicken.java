16369c363375363e35006341383628f7fe713ac0
TheBigEye <thebigeyedev@gmail.com>
2021-05-06 16:17:57 -0300

    Animals release heart particles when attracted with food

--- src/minicraft/entity/mob/Chicken.java
+++ src/minicraft/entity/mob/Chicken.java
@@ -1,52 +1,97 @@
 package minicraft.entity.mob;
 
+import java.util.Random;
+
 import minicraft.core.Game;
 import minicraft.core.io.Settings;
+import minicraft.entity.particle.HeartParticle;
 import minicraft.gfx.MobSprite;
 import minicraft.item.Items;
 
 public class Chicken extends PassiveMob {
 	private static MobSprite[][] sprites = MobSprite.compileMobSpriteAnimations(0, 32);
 	
+	private Random rnd = new Random();
+	
 	/**
 	 * Creates a Chicken.
 	 */
+	
 	public Chicken() {
 		super(sprites);
 	}
 	
 	public void tick() {
 		super.tick();
-		
+
+		// Drop eggs each 15 secs
 		int min = 0, max = 0;
-		if (Settings.get("diff").equals("Passive")) {min = 1; max = 2;}
-		if (Settings.get("diff").equals("Easy")) {min = 1; max = 2;}
-		if (Settings.get("diff").equals("Normal")) {min = 1; max = 1;}
-		if (Settings.get("diff").equals("Hard")) {min = 0; max = 1;}
-		
-		if (random.nextInt(1500)==1 && Game.isMode("Survival")) { //drop eggs each 15 secs
-			
-			dropItem(min, max, Items.get("egg"));
-			
+		if (Settings.get("diff").equals("Passive")) {
+			min = 1;
+			max = 2;
 		}
-		
+		if (Settings.get("diff").equals("Easy")) {
+			min = 1;
+			max = 2;
+		}
+		if (Settings.get("diff").equals("Normal")) {
+			min = 1;
+			max = 1;
+		}
+		if (Settings.get("diff").equals("Hard")) {
+			min = 0;
+			max = 1;
+		}
+
+		if (random.nextInt(1500) == 1 && Game.isMode("Survival")) { // drop eggs each 15 secs
+
+			dropItem(min, max, Items.get("egg"));
+
+		}
+
 		Player player = getClosestPlayer();
-		if (player != null && player.activeItem != null && player.activeItem.name.equals("Seeds")){ //This function will make the entity follow the player directly
+		if (player != null && player.activeItem != null && player.activeItem.name.equals("Seeds")) { // This function will make the entity follow the player directly
+
+			// Render heart particles
+			int randX = rnd.nextInt(10);
+			int randY = rnd.nextInt(9);
+
+			if (random.nextInt(12) == 1) {
+				level.add(new HeartParticle(x - 9 + randX, y - 12 + randY));
+			}
+			if (random.nextInt(12) == 4) {
+
+			}
+			if (random.nextInt(12) == 6) {
+				level.add(new HeartParticle(x - 9 + randX, y - 12 + randY));
+			}
+			if (random.nextInt(12) == 8) {
+
+			}
+			if (random.nextInt(12) == 12) {
+				level.add(new HeartParticle(x - 9 + randX, y - 12 + randY));
+			}
+
 			int xd = player.x - x;
 			int yd = player.y - y;
-				/// if player is less than 6.25 tiles away, then set move dir towards player
-				int sig0 = 1; // this prevents too precise estimates, preventing mobs from bobbing up and down.
-				xa = ya = 0;
-				if (xd < sig0) xa = -1;
-				if (xd > sig0) xa = +1;
-				if (yd < sig0) ya = -1;
-				if (yd > sig0) ya = +1;
-			} else {
-				// if the Pet was following the player, but has now lost it, it stops moving.
-					//*that would be nice, but I'll just make it move randomly instead.
-				randomizeWalkDir(false);
-			}
+
+			/// if player is less than 6.25 tiles away, then set move dir towards player
+			int sig0 = 1; // this prevents too precise estimates, preventing mobs from bobbing up and down.
+			xa = ya = 0;
+			if (xd < sig0)
+				xa = -1;
+			if (xd > sig0)
+				xa = +1;
+			if (yd < sig0)
+				ya = -1;
+			if (yd > sig0)
+				ya = +1;
+		} else {
+			// if the Pet was following the player, but has now lost it, it stops moving.
+			// *that would be nice, but I'll just make it move randomly instead.
+			randomizeWalkDir(false);
 		}
+	}
 	
 	public void die() {
 		int min = 0, max = 0;
--- src/minicraft/entity/mob/Sheep.java
+++ src/minicraft/entity/mob/Sheep.java
@@ -1,10 +1,16 @@
 package minicraft.entity.mob;
 
+import org.jetbrains.annotations.Nullable;
+
 import minicraft.core.Updater;
 import minicraft.core.io.Settings;
+import minicraft.entity.Direction;
 import minicraft.gfx.MobSprite;
 import minicraft.gfx.Screen;
+import minicraft.item.Item;
 import minicraft.item.Items;
+import minicraft.item.ToolItem;
+import minicraft.item.ToolType;
 import minicraft.level.tile.Tile;
 import minicraft.level.tile.Tiles;
 
@@ -14,22 +20,23 @@ public class Sheep extends PassiveMob {
 
 	private static final int WOOL_GROW_TIME = 3 * 60 * Updater.normSpeed; // Three minutes
 
-	public boolean cut = false;
+	// Cut
+	public boolean isCut = false;
 	private int ageWhenCut = 0;
-	
+
 	/**
 	 * Creates a sheep entity.
 	 */
 	public Sheep() {
 		super(sprites);
 	}
-	
+
 	@Override
 	public void render(Screen screen) {
 		int xo = x - 8;
 		int yo = y - 11;
 
-		MobSprite[][] curAnim = cut ? cutSprites : sprites;
+		MobSprite[][] curAnim = isCut ? cutSprites : sprites;
 
 		MobSprite curSprite = curAnim[dir.getDir()][(walkDist >> 3) % curAnim[dir.getDir()].length];
 		if (hurtTime > 0) {
@@ -39,53 +46,67 @@ public class Sheep extends PassiveMob {
 		}
 	}
 
-	
+
 	public void tick() {
 		super.tick();
-		
-		if (age - ageWhenCut > WOOL_GROW_TIME) cut = false;
-		
+
+		if (age - ageWhenCut > WOOL_GROW_TIME)
+			isCut = false;
+
 		Player player = getClosestPlayer();
-		if (player != null && player.activeItem != null && player.activeItem.name.equals("Wheat")){ //This function will make the entity follow the player directly
+		if (player != null && player.activeItem != null && player.activeItem.name.equals("Wheat")) { // This function will make the entity follow the player directly
 			int xd = player.x - x;
 			int yd = player.y - y;
-				/// if player is less than 6.25 tiles away, then set move dir towards player
-				int sig0 = 1; // this prevents too precise estimates, preventing mobs from bobbing up and down.
-				xa = ya = 0;
-				if (xd < sig0) xa = -1;
-				if (xd > sig0) xa = +1;
-				if (yd < sig0) ya = -1;
-				if (yd > sig0) ya = +1;
-			} else {
-				// if the Pet was following the player, but has now lost it, it stops moving.
-					//*that would be nice, but I'll just make it move randomly instead.
-				randomizeWalkDir(false);
-			}
-		
+			
+			/// if player is less than 6.25 tiles away, then set move dir towards player
+			int sig0 = 1; // this prevents too precise estimates, preventing mobs from bobbing up and down.
+			xa = ya = 0;
+			if (xd < sig0)
+				xa = -1;
+			if (xd > sig0)
+				xa = +1;
+			if (yd < sig0)
+				ya = -1;
+			if (yd > sig0)
+				ya = +1;
+		} else {
+			// if the Pet was following the player, but has now lost it, it stops moving.
+			// *that would be nice, but I'll just make it move randomly instead.
+			randomizeWalkDir(false);
+		}
+
 		Tile tile = level.getTile(x >> 4, y >> 4);
-		if ( tile == Tiles.get("snow")) {
+		if (tile == Tiles.get("snow")) {
 			remove();
 			level.add(new Goat(), x, y);
-			
+
 		}
-		}
-	
-	public void shear() {
-		cut = true;
-		dropItem(1, 3, Items.get("Wool"));
-		ageWhenCut = age;
 	}
-	
+
+	public boolean interact(Player player, @Nullable Item item, Direction attackDir) {
+		if (isCut) return false;
+
+		if (item instanceof ToolItem) {
+			if (((ToolItem) item).type == ToolType.Shear) {
+				isCut = true;
+				dropItem(1, 3, Items.get("Wool"));
+				ageWhenCut = age;
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public void die() {
 		int min = 0, max = 0;
 		if (Settings.get("diff").equals("Passive")) {min = 1; max = 3;}
 		if (Settings.get("diff").equals("Easy")) {min = 1; max = 3;}
 		if (Settings.get("diff").equals("Normal")) {min = 1; max = 2;}
 		if (Settings.get("diff").equals("Hard")) {min = 0; max = 2;}
-		
-		if (!cut) dropItem(min, max, Items.get("wool"));
+
+		if (!isCut) dropItem(min, max, Items.get("wool"));
 		dropItem(min, max, Items.get("Raw Beef"));
-		
+
 		super.die();
 	}
 }
